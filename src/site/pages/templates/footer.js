d7b3c65115c891aebee5264d97aeb3a1db876fbe
Ceki Gulcu <ceki@qos.ch>
2023-05-02 17:56:56 +0200

    update copyright year, correct L. Martins name
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- src/site/pages/templates/footer.js
+++ src/site/pages/templates/footer.js
@@ -3,7 +3,7 @@ document.write('<table class="footer" border="0">')
 
 document.write('<tr>')
 
-document.write('  <td valign="top" align="left">Copyright &copy; 2004-2022  <a href="http://www.qos.ch/">QOS.ch</a></td>')
+document.write('  <td valign="top" align="left">Copyright &copy; 2004-2023  <a href="http://www.qos.ch/">QOS.ch</a></td>')
 
 //document.write('  <td rowspan="2">');
 //document.write('    <a href="http://twitter.com/qos_ch">');
@@ -15,7 +15,7 @@ document.write('  <td valign="top" align="left">Copyright &copy; 2004-2022  <a h
 AAT = '@'
 DOOTT = '.'
 document.write('<td align="right" >')
-document.write('Logo created by <a href="' + prefix +'count/lisette.html">Lisette Martins</a>');
+document.write('Logo created by <a href="' + prefix +'count/lisette.html">Lise Martins</a>');
 //document.write('Logo created by <a href="https://www.facebook.com/Lise-Martins-Graphic-Designer-102159252581148/">Lisette Martins</a>');
 document.write('</td>')
 document.write('</tr>')

b9823290dc302788395590f1105ea8e51829b860
Ceki Gulcu <ceki@qos.ch>
2024-04-12 18:18:27 +0200

    version 2.0.13 released
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- src/site/pages/templates/footer.js
+++ src/site/pages/templates/footer.js
@@ -3,23 +3,18 @@ document.write('<table class="footer" border="0">')
 
 document.write('<tr>')
 
-document.write('  <td valign="top" align="left">Copyright &copy; 2004-2024  <a href="http://www.qos.ch/">QOS.ch</a></td>')
-
-//document.write('  <td rowspan="2">');
-//document.write('    <a href="http://twitter.com/qos_ch">');
-//document.write('      <img alt="Follow @qos_ch" src="images/follow_us.png" />');
-//document.write('    </a>');
-//document.write('  </td>');
+document.write('<td valign="top" align="left">');
+document.write('Copyright &copy; 2004-2024 <a href="http://www.qos.ch/">QOS.ch Sarl (Switzerland)</a>');
+document.write('<br/>');
+document.write('This document is licensed under a <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons BY-SA License</a>');
+document.write('</td>');
 
 
-AAT = '@'
-DOOTT = '.'
 document.write('<td align="right" >')
 document.write('Logo created by <a href="' + prefix +'count/lisette.html">Lise Martins</a>');
-//document.write('Logo created by <a href="https://www.facebook.com/Lise-Martins-Graphic-Designer-102159252581148/">Lisette Martins</a>');
 document.write('</td>')
 document.write('</tr>')
-
+  
 
 document.write('</table>')
  
